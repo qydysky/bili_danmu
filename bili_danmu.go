42dc131ac81193234927668c878f82554749e38d
qydysky <qydysky@foxmail.com>
2020-11-14 10:09:28 +0800

    fix gtk

--- bili_danmu.go
+++ bili_danmu.go
@@ -93,9 +93,8 @@ func Demo(roomid ...int) {
 						if p.Checkfile().IsExist("cookie.txt") {//附加功能 弹幕机
 							reply.Danmuji_auto(1)
 						}
-						{//附加功能 直播流保存 Qt窗口 Gtk窗口
+						{//附加功能 直播流保存 Gtk窗口
 							go reply.Saveflvf()
-							go reply.Qtdf()
 							go reply.Gtkf()
 						}
 					}()
