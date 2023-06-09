5003e9167e3f2b101baa48296cea19f358e54680
qydysky <qydysky@foxmail.com>
2023-06-09 01:03:14 +0000

    Fix 读超时

--- Reply/stream.go
+++ Reply/stream.go
@@ -796,7 +796,7 @@ func (t *M4SStream) saveStreamFlv() (e error) {
 				NoResponse:       true,
 				Async:            true,
 				Proxy:            t.common.Proxy,
-				WriteLoopTO:      readTO * 2 + 1,
+				WriteLoopTO:      int(readTO) * 2 + 1,
 				Header: map[string]string{
 					`Host`:            surl.Host,
 					`User-Agent`:      `Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:103.0) Gecko/20100101 Firefox/103.0`,
