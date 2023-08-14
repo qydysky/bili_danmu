08773140d80108bdca88689658bc0a40083dc176
qydysky <qydysky@foxmail.com>
2023-08-15 05:59:41 +0800

    Fix  未生成xml #86

--- Reply/F/comp.go
+++ Reply/F/comp.go
@@ -8,10 +8,10 @@ import (
 
 func init() {
 	var linkMap = map[string][]string{
-		"github.com/qydysky/Reply.startRecDanmu.stop": {
+		"github.com/qydysky/bili_danmu/Reply.startRecDanmu.stop": {
 			comp.Sign[danmuXml.Sign](),
 		},
-		"github.com/qydysky/Reply.preparing": {
+		"github.com/qydysky/bili_danmu/Reply.preparing": {
 			comp.Sign[liveOver.Sign](),
 		},
 	}
