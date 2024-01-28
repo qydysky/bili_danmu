1169a32ec9a82837c9a0480b3eb2b9f3229a73e9
qydysky <qydysky@foxmail.com>
2024-01-28 22:29:07 +0800

    Fix github Action Error #100

--- Json/getRoomBaseInfo.go
+++ Json/getRoomBaseInfo.go
@@ -10,25 +10,25 @@ type GetRoomBaseInfo struct {
 }
 
 type GetRoomBaseInfoD struct {
-	RoomID         int    `json:"room_id"`
-	UID            int    `json:"uid"`
-	AreaID         int    `json:"area_id"`
-	LiveStatus     int    `json:"live_status"`
+	RoomID     int `json:"room_id"`
+	UID        int `json:"uid"`
+	AreaID     int `json:"area_id"`
+	LiveStatus int `json:"live_status"`
 	// LiveURL        string `json:"live_url"`
-	ParentAreaID   int    `json:"parent_area_id"`
-	Title          string `json:"title"`
+	ParentAreaID int    `json:"parent_area_id"`
+	Title        string `json:"title"`
 	// ParentAreaName string `json:"parent_area_name"`
 	// AreaName       string `json:"area_name"`
-	LiveTime       string `json:"live_time"`
+	LiveTime string `json:"live_time"`
 	// Description    string `json:"description"`
 	// Tags           string `json:"tags"`
-	Attention      int    `json:"attention"`
-	Online         int    `json:"online"`
-	ShortID        int    `json:"short_id"`
-	Uname          string `json:"uname"`
-	Cover          string `json:"cover"`
-	Background     string `json:"background"`
-	JoinSlide      int    `json:"join_slide"`
-	LiveID         int64  `json:"live_id"`
-	LiveIDStr      string `json:"live_id_str"`
+	Attention  int    `json:"attention"`
+	Online     int    `json:"online"`
+	ShortID    int    `json:"short_id"`
+	Uname      string `json:"uname"`
+	Cover      string `json:"cover"`
+	Background string `json:"background"`
+	JoinSlide  int    `json:"join_slide"`
+	LiveID     int64  `json:"live_id"`
+	LiveIDStr  string `json:"live_id_str"`
 }
