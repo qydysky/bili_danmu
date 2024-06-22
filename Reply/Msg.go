a9ea00ab2a917c122a519811c0f1099c5a88ddf4
qydysky <qydysky@foxmail.com>
2024-06-22 13:49:17 +0000

    Add msg POPULARITY_RANK_TAB_CHG

--- Reply/Msg.go
+++ Reply/Msg.go
@@ -17,6 +17,7 @@ var msglog = c.C.Log.Base(`Msg`)
 
 // Msg类型数据处理方法map
 var Msg_map = map[string]func(replyF, string){
+	`POPULARITY_RANK_TAB_CHG`:           nil,
 	`POPULAR_RANK_GUIDE_CARD`:           nil, //投喂一个人气票帮助主播打榜
 	`PK_BATTLE_SETTLE_NEW`:              nil,
 	`LIKE_GUIDE_USER`:                   nil,              //主播@你：点点赞支持一下我吧
