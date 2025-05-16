8c0b8d57ba47c4eec3d342d0d2134047990ced31
qydysky <qydysky@foxmail.com>
2025-05-16 19:26:40 +0800

    Fix date race (#207)

--- Reply/Reply.go
+++ Reply/Reply.go
@@ -115,6 +115,8 @@ func Reply(common *c.Common, b []byte) {
 	}
 
 	replyFS := replyF{common}
+	defer replyFS.Lock()()
+
 	for len(b) != 0 {
 		head := F.HeadChe(b[:c.WS_PACKAGE_HEADER_TOTAL_LENGTH])
 		if int(head.PackL) > len(b) {
