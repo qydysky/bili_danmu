c6efb5b7de1e8a738a4b8ba2b9d31f2ef6b6ead8
qydysky <qydysky@foxmail.com>
2026-01-30 02:39:19 +0800

    Fix

--- Reply/decoder/common.go
+++ Reply/decoder/common.go
@@ -1,5 +1,9 @@
 package decoder
 
-import perrors "github.com/qydysky/part/errors"
+import (
+	perrors "github.com/qydysky/part/errors"
+
+	_ "github.com/qydysky/bili_danmu/Reply/F/videoFastSeed" //removable
+)
 
 var ErrDecode = perrors.Action("ErrDecode")
